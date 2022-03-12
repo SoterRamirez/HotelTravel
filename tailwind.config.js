74d384c8b9d3b4a28c8cab8b51bf2724199540de
Soter Ramirez <soterrc@gmail.com>
2022-03-12 13:13:10 -0600

    Imagenes, y conf de tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,40 @@
+// const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{html,js}"],
   theme: {
     colors: {
-      primary: colors.indigo,
-      secondary: colors.yellow,
-      neutral: colors.gray,
+      primario: "#CC2D4A",
+      secundario: "#8FA206",
+      terciario: "#61AEC9",
     },
-    extend: {},
+    extend: {
+      backgroundImage: {
+        sanFrancisco: "url('../img/sanFrancisco.jpg')",
+        sanFranciscoDesktop: "url('../img/sanFranciscoDesktop.jpg')",
+        yosemite: "url('../img/yosemite.jpg')",
+        LA: "url('../img/LA.jpg')",
+        seattle: "url('../img/seattle.jpg')",
+        new_york: "url('../img/new_york.jpg')",
+        norway: "url('../img/norway.jpg')",
+        sydney: "url('../img/sydney.jpg')",
+        miami: "url('../img/miami.jpg')",
+        switzerland: "url('../img/switzerland.jpg')",
+        bali: "url('../img/bali.jpg')",
+        norway: "url('../img/norway.jpg')",
+        chicago: "url('../img/chicago.jpg')",
+        europe: "url('../img/europe.jpg')",
+        iceland: "url('../img/iceland.jpg')",
+      },
+      textColor: {
+        principal: "#CC2D4A",
+        secondary: "#8FA206",
+        tertiary: "#61AEC9",
+      },
+    },
+    fontFamily: {
+      Montserrat: ["Montserrat", "sans-serif"],
+    },
   },
   plugins: [
     require("@tailwindcss/forms"),
