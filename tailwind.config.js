cb20ffcce0f1d78ce3f5912d0446b2756e12598c
Soter Ramirez <soterrc@gmail.com>
2022-03-12 12:03:24 -0600

    Pruebas

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   content: ["./public/index.html", "./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      primary: colors.indigo,
+      secondary: colors.yellow,
+      neutral: colors.gray,
+    },
     extend: {},
   },
   plugins: [],
